3d8781b048ffb972bca584e5068d5f932ce89a7f
Floris Bernard <floris.bernard@gmail.com>
2021-07-19 00:15:10 +0200

    Add date picker

--- js/reservation-form.js
+++ js/reservation-form.js
@@ -2,10 +2,86 @@
 const ENDPOINT =
   "https://script.google.com/macros/s/AKfycbxmEwTD9e9XPo50RJhBDTMUZQqGVmdsROFmESKCT_fmM911JM6CKnNjoBwTqKhdPKtL/exec";
 
+const DEFAULT_EVENT_TIME = "15:00:00";
+
+const mainContentWrapper = document.querySelector("#main-content");
 const reservationForm = document.querySelector("#reservation");
-const reservationFormSubmit = reservationForm.querySelector(
-  'button[type="submit"]'
-);
+const dateSelector = reservationForm.querySelector("#select-date");
+
+function setupForm() {
+  for (const option of dateSelector.querySelectorAll("option")) {
+    const optionValue = option.getAttribute("value");
+    const optionDate = Date.parse(`${optionValue}T${DEFAULT_EVENT_TIME}`);
+
+    if (optionDate < Date.now()) {
+      dateSelector.removeChild(option);
+    }
+  }
+
+  if (!dateSelector.querySelectorAll("option").length) {
+    switchTemplate("no-dates");
+    return;
+  }
+
+  const defaultOption = dateSelector.querySelector('option[data-allow-default]');
+  if (defaultOption) {
+    dateSelector.value = defaultOption.getAttribute('value');
+  }
+  
+  selectToRadioList(dateSelector);
+
+  reservationForm.addEventListener("submit", handleReservationSubmit);
+}
+
+function selectToRadioList(selectInput) {
+  const selectName = selectInput.getAttribute('name');
+  const radioList = document.createElement('div');
+  radioList.className = 'select-radio-list';
+  selectInput.parentElement.insertBefore(radioList, selectInput.nextSibling);
+
+  const radioInputs = Array.from(selectInput.querySelectorAll('option')).map(option => {
+    const optionValue = option.getAttribute('value');
+    const label = document.createElement('label');
+    label.innerHTML = option.innerHTML;
+    label.setAttribute('for', `${selectName}-${optionValue}`);
+    const radioInput = document.createElement('input');
+    radioInput.setAttribute('type', 'radio');
+    radioInput.setAttribute('value', optionValue);
+    radioInput.setAttribute('name', `${selectName}-radio`);
+    radioInput.setAttribute('id', `${selectName}-${optionValue}`);
+    radioList.appendChild(radioInput);
+    radioList.appendChild(label);
+    radioInput.addEventListener('change', handleRadioChange);
+    return radioInput;
+  });
+
+  function updateSelected() {
+    for (const radioInput of radioInputs) {
+      radioInput.checked = radioInput.value === selectInput.value;
+    }
+  }
+
+  function handleRadioChange(e) {
+    selectInput.value = e.currentTarget.value;
+  }
+
+  function handleSelectChange() {
+    updateSelected();
+  }
+
+  selectInput.addEventListener('change', handleSelectChange);
+
+  updateSelected();
+}
+
+function switchTemplate(templateId) {
+  const template = document.querySelector(`#${templateId}`);
+  while (mainContentWrapper.firstChild) {
+    mainContentWrapper.removeChild(mainContentWrapper.firstChild);
+  }
+
+  mainContentWrapper.appendChild(template.content.cloneNode(true));
+}
 
 function handleReservationSubmit(e) {
   e.preventDefault();
@@ -27,5 +103,4 @@ function handleReservationSubmit(e) {
     });
 }
 
-reservationFormSubmit.removeAttribute("disabled");
-reservationForm.addEventListener("submit", handleReservationSubmit);
+setupForm();
--- testtafel/reserveren.md
+++ testtafel/reserveren.md
@@ -3,10 +3,37 @@ title: TestTafel - Reserveren
 layout: default
 language: nl
 translation: testtafel
+scripts:
+  - reservation-form
+stylesheets:
+  - forms
 ---
+
+<template id="no-dates">
+	<p>
+		Er zijn momenteel geen TestTafel edities waarvoor je kan reserveren. Probeer het later nog eens!
+	</p>
+</template>
 
 <div class="row">
 	<div class="container">
 		<h1>Reserveren voor TestTafel</h1>
+
+		<div id="main-content">
+			<form id="reservation">
+				<select id="select-date" class="cloak" name="date">
+					{% for event in site.data.testtafels %}
+						{% assign weekday = event.date | date: "%u" | minus: 1 %}
+						{% assign day = event.date | date: "%e" %}
+						{% assign month = event.date | date: "%-m" | minus: 1 %}
+						{% assign year = event.date | date: "%Y" %}
+
+						<option value="{{ event.date }}"{% unless event.fullyBooked %} data-allow-default{% endunless %}>
+							{{ site.data.i18n.weekdays-nl[weekday] | capitalize }} {{ day }} {{ site.data.i18n.months-nl[month] }}{% if event.fullyBooked %} (volgeboekt){% endif %}
+						</option>
+					{% endfor %}
+				</select>
+			</form>
+		</div>
     </div>
 </div>
