4b8ee98575e5281fc2a48fcec76497fbff225df8
Floris Bernard <floris.bernard@gmail.com>
2021-08-11 19:21:10 +0200

    Update reservation form target

--- js/src/reservation-form/config.js
+++ js/src/reservation-form/config.js
@@ -1,5 +1,5 @@
 export const ENDPOINT =
-"https://script.google.com/macros/s/AKfycbwWS-uH1haqSf2Isgj60wSpsWGW6su6-G0_XgHgtu6ZpPLvjXpC0xvfZyyADi3iv7VavQ/exec";
+"https://script.google.com/macros/s/AKfycbzVKupTe9lTmZfARjtO7-iNsOeBGBFYhJjQSRdp5huDQJ2JAp-M8kNcn24-3gkpj7bM/exec";
 
 export const MIN_PEOPLE_OWN_TABLE = 5;
 export const SHARED_TABLE_START_TIME = "19:00";
