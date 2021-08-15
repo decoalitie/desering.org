406c476ffeed9a6423321eb570894e15bef23f38
Floris Bernard <floris.bernard@gmail.com>
2021-08-15 21:09:30 +0200

    Switch rollup to webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+    return {
+        entry: path.resolve(__dirname, 'js/src/reservation-form/main.js'),
+        output: {
+            filename: `reservation-form${argv.mode === 'development' ? '-dev' : ''}.js`,
+            path: path.resolve(__dirname, 'js/dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            cacheDirectory: true
+                        }
+                    }
+                }
+            ]
+        },
+    };
+};
