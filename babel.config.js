c4733e6ef67f6a1659b467fc1532332fc82a32f9
Floris Bernard <floris.bernard@gmail.com>
2021-08-15 21:31:47 +0200

    Remove babel config override in webpack config

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = {
                 targets: 'last 2 versions, >0.01% in NL and not dead and supports css-variables',
                 useBuiltIns: 'usage',
                 corejs: '3.16',
-                shippedProposals: true
+                shippedProposals: true,
+                bugfixes: true
             }
         ]
     ],
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = (env, argv) => {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env'],
                             cacheDirectory: true
                         }
                     }
